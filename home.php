64b9b5961983011a370da62badffc563eb1680b2
jijerry <jipengbo@xunlei.com>
2018-06-04 16:09:44 +0800

    页眉页脚单独文件

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerry
+ * Date: 2018/6/4
+ * Jerry 公司主页PHP脚本
+ */
+
+require 'header.php';
+?>
+
+<!-- page content -->
+<p>Welcome to the home of Jerry Consulting.
+    Please take some time to get to know us.</p>
+<p>We specialize in serving your business needs
+    and hope to hear from you soon.</p>
+
+<?php
+require 'footer.php';
+?>
+
+
