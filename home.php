fdf83e393357d6f3a1d3429a5716f236482fe452
jijerry <jipengbo@xunlei.com>
2018-06-08 15:41:33 +0800

    网页类，继承类创建网页

--- home.php
+++ home.php
@@ -1,22 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jerry
- * Date: 2018/6/4
- * Jerry 公司主页PHP脚本
- */
+///**
+// * Created by PhpStorm.
+// * User: jerry
+// * Date: 2018/6/4
+// * Jerry 公司主页PHP脚本
+// */
+//
+//require 'header.php';
+//?>
+<!---->
+<!--     page content -->
+<!--    <p>Welcome to the home of Jerry Consulting.-->
+<!--        Please take some time to get to know us.</p>-->
+<!--    <p>We specialize in serving your business needs-->
+<!--        and hope to hear from you soon.</p>-->
+<!---->
+<?php
+//require 'footer.php';
+//?>
 
-require 'header.php';
-?>
 
-<!-- page content -->
-<p>Welcome to the home of Jerry Consulting.
-    Please take some time to get to know us.</p>
-<p>We specialize in serving your business needs
-    and hope to hear from you soon.</p>
 
 <?php
-require 'footer.php';
-?>
+require 'page.inc';
+$homepage = new Page();
+$homepage -> content = '<p>Welcome to the home of Jerry Consulting.
+                        Please take some time to get to know us.</p>
+                        <p>We specialize in serving your business needs
+                        and hope to hear from you soon.</p>';
+$homepage -> Display();
+
 
 
